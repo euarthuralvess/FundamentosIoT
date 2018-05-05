ac86ae0dc81f975e66a3b88ac4dfba30cb3c6e11
Arthur Alves <32318596+arthur-alvess@users.noreply.github.com>
2018-05-05 14:13:14 -0300

    Create questao1.c

---
+++ Lista1C/questao1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>  
+ 
+ int main(){  
+   printf("\n Programa que gera todos os numeros pares e multiplos de 03. \n\t");  
+   
+   int cont,num1,num2;  
+   printf("\nDigite o num 1: \t");  
+   scanf("%d",&valor1);  
+   printf("Digite o num 2: \t");  
+   scanf("%d",&num2);  
+   
+   if(num2<num1){
+   	 cont=num1;  
+     num1=num2;  
+     num2=cont;  
+   }  
+   for(cont=num1;cont<=num2;cont++){  
+     if(cont%2==0){  
+       printf("%d \n",cont);  
+     }   
+	}
+	
+   system("pause");
+   
+   int n,i;  
+   printf("\nDigite o total de números multiplos de 3: \t");  
+   scanf("%i", &n);
+   i = 1;
+   while(i<=n){
+	if(i%3==0){
+   		printf("%i \n", i);
+   	}
+	   i++;
+	}
+	
+	system("pause");
+	
+	return 0;
+} 
