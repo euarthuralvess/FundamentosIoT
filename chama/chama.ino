52e5c75f18b09fdf0be695b38e9c55dfe191e421
Djalma Neto <djalmaneto@MacBook-Pro-de-Djalma.local>
2018-10-02 13:51:48 -0300

    Update to English in some codes

--- chama/chama.ino
+++ chama/chama.ino
@@ -1,15 +1,15 @@
 const int PINO_D0 = 9;
 const int PINO_A0 = A1;
-const int LED_VERDE=11;
-const int LED_VERMELHO=12;
+const int LED_GREEN=11;
+const int LED_RED=12;
  
 void setup()
 {
   Serial.begin(9600);
   pinMode(PINO_A0, INPUT);
   pinMode(PINO_D0, INPUT);
-  pinMode(LED_VERDE, OUTPUT);
-  pinMode(LED_VERMELHO,OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_RED,OUTPUT);
 }
  
 void loop()
@@ -17,19 +17,19 @@ void loop()
   int valorAferido = analogRead(PINO_A0);
   int valorSensor= digitalRead(PINO_D0);
  
-  Serial.print("Porta analogica: ");
+  Serial.print("Analogic Input: ");
   Serial.print(valorAferido);
 
  
   if (valorSensor != HIGH)
   {
-    Serial.println("Fogo detectado !!!");
-    digitalWrite(LED_VERMELHO,HIGH);
-    digitalWrite(LED_VERDE,LOW);
+    Serial.println("ITS ON FIRE !!!");
+    digitalWrite(LED_RED,HIGH);
+    digitalWrite(LED_GREEN,LOW);
   }else{
-    Serial.println("Sem fogo");
-    digitalWrite(LED_VERMELHO,LOW);
-    digitalWrite(LED_VERDE,HIGH);
+    Serial.println("Chill AF");
+    digitalWrite(LED_RED,LOW);
+    digitalWrite(LED_GREEN,HIGH);
   }
   delay(500);
 }
